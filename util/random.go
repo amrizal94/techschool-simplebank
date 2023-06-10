8ec4fab7d75babf26c7158b9d9d49e1f58479912
Ari Muhammad Rizal <arimrizal94@gmail.com>
2023-06-10 17:54:08 +0700

    test: account_test (create, get, list, update, delete)

---
+++ util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// randomInt generates a random integer between min and max
+func randomInt(min, max int64) int64 {
+	return min + r.Int63n(max-min+1)
+}
+
+// randomString generates a random string of length n
+func randomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[r.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return randomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return randomInt(0, 1000)
+}
+
+// RandomCurrecy generates a random currency code
+func RandomCurrecy() string {
+	currencies := []string{"EUR", "USD", "IDR"}
+	n := len(currencies)
+	return currencies[r.Intn(n)]
+}
