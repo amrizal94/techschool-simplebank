58f1da3b16302761c15b7a0f31492bf7fcd5cff7
Ari Muhammad Rizal <arimrizal94@gmail.com>
2023-06-27 08:52:16 +0700

    migratian: add table users and handle DB errors

--- api/account.go
+++ api/account.go
@@ -5,6 +5,7 @@ import (
 
 	db "github.com/amrizal94/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -27,6 +28,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(403, gin.H{"error": err.Error()})
+				return
+			}
+		}
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
